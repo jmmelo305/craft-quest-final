1c866c037f92607359d463e985262ea2e9ce5581
Melo <jmmelo.edu@gmail.com>
2026-04-27 10:18:32 -0400

    Finished tile class, started on Inventory class

--- CraftQuestApp/Model.java
+++ CraftQuestApp/Model.java
@@ -54,11 +54,45 @@ class Tile {
         return type != Type.WATER;
     }
 
-    // implement the check tile collectability if the tile is only a chest
+    // Method to check if the tile is collectible only if it's a CHEST and it hasn't been collected yet.
+    // && means that both conditions must be true, and the !collected means that collected is false. 
+    boolean isCollectible(){
+        return type == Type.CHEST && !collected; 
+    }
 
-    // create method called collect that gets called when the player steps on a chest
+    // method that gets called when the play steps on a chest.
+    void collect (){
+        if (type == Type.CHEST){
+            collected = true; 
+        }
+    }
+    
+    // Switch case to draw the corresponding tile on the screen
+    // depending on the tile type
+    // collected  ? "o" : "C"
+    // represents that if the chest has been collected it returns "o", but if it hasn't it returns "C"
+    String toSymbol(){
+        switch(type){
+            case GRASS: return ".";
+            case STONE: return "#";
+            case WATER: return "~";
+            case CHEST: return collected ? "o" : "C";
+            default:    return "?";
+        }
+    }
+}
 
-    // .....
+class Inventory {
+// The only job of this class is to store a list of the items the player has collected. 
+    
+// List to store items, only strings. 
+    private List <String> items;
 
+    // Constructor 
+    Inventory (){ 
+        this.items = new ArrayList<>();
+    }
+
+    
 
 }
